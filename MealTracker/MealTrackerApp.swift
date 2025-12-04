56ffd3b7c9e232b1228999fb9307678400c3d1bd
Carlos Campos <ceccontreras5@gmail.com>
2025-12-03 21:18:31 -0500

    Initial Commit

---
+++ MealTracker/MealTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealTrackerApp.swift
+//  MealTracker
+//
+//  Created by Carlos Campos on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MealTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
